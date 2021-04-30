8ab7b3a8e8583eb053756e90bee17ba80e740dfb
dhimashary <dhimas.hary@gmail.com>
2021-04-30 13:09:19 +0700

    feat:SignIn And SignUp component

--- challenge-2/src/App.vue
+++ challenge-2/src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <h1>{{ message }}</h1>
+  <div>
+    <div v-if="currentPage === 'LoginPage'" class="hidden">
+      <LoginPage  @changePage="changePage"/>
+    </div>
+    <div v-if="currentPage === 'RegisterPage'" class="hidden">
+      <RegisterPage @changePage="changePage" />
+    </div>
+  </div>
 </template>
 
 <script>
+import LoginPage from "./views/LoginPage";
+import RegisterPage from "./views/RegisterPage";
+
 export default {
+  name: "app",
   data() {
     return {
-      message: 'Hello world',
+      message: "Hello world",
+      currentPage: "LoginPage"
     };
   },
+  components: {
+    LoginPage,
+    RegisterPage
+  },
+  methods: {
+    changePage(page) {
+      console.log(page, "called")
+      this.currentPage = page
+    }
+  }
 };
 </script>
 
---
+++ challenge-2/src/views/RegisterPage.vue
@@ -0,0 +1,150 @@
+<template>
+  <!-- 
+    =======================================================================
+    Name    :   Simple Sign In
+    Author  :   Surjith S M
+    Twitter :   @surjithctly
+
+    Get more components here 👉 https://web3templates.com/components
+
+    Copyright © 2021
+    =======================================================================
+  -->
+  <div class="flex items-center min-h-screen bg-white dark:bg-gray-900">
+    <div class="container mx-auto">
+      <div class="max-w-md mx-auto my-10">
+        <div class="text-center">
+          <h1
+            class="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200"
+          >
+            Sign Up
+          </h1>
+          <p class="text-gray-500 dark:text-gray-400">
+            Sign Up to access your account
+          </p>
+        </div>
+        <div class="m-7">
+          <form @submit.prevent="signIn">
+            <div class="mb-6">
+              <label
+                for="email"
+                class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                >Email Address</label
+              >
+              <input
+                type="email"
+                name="email"
+                id="email"
+                v-model="email"
+                placeholder="you@company.com"
+                class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+              />
+            </div>
+            <div class="mb-6">
+              <div class="flex justify-between mb-2">
+                <label
+                  for="password"
+                  class="text-sm text-gray-600 dark:text-gray-400"
+                  >Password</label
+                >
+                <a
+                  href="#!"
+                  class="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300"
+                  >Forgot password?</a
+                >
+              </div>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                v-model="password"
+                placeholder="Your Password"
+                autocomplete="off"
+                pattern=".{6,13}"
+                required
+                title="6 to 13 characters minimum"
+                class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+              />
+            </div>
+            <div class="mb-6">
+              <button
+                type="submit"
+                class="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+              >
+                Sign Up
+              </button>
+            </div>
+            <p class="text-sm text-center text-gray-400">
+              Already have an account yet?
+              <a
+                href="#!"
+                @click="$emit('changePage', 'LoginPage')"
+                class="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
+                >Sign In</a
+              >.
+            </p>
+            <p class="text-sm text-center text-gray-400 mb-3">
+              Or Sign In with Google
+            </p>
+
+            
+            <GoogleLogin
+              :params="params"
+              :renderParams="renderParams"
+              :onSuccess="onSuccess"
+              :onFailure="onFailure"
+              >LOGIN</GoogleLogin
+            >
+            <GoogleLogin :params="params" :logoutButton="true"
+              >NANTI APUS</GoogleLogin
+            >
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GoogleLogin from "vue-google-login";
+
+export default {
+  name: "RegisterPage",
+  data() {
+    return {
+      email: "",
+      password: "",
+      params: {
+        client_id: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+      },
+      // only needed if you want to render the button with the google ui
+      renderParams: {
+        width: 380,
+        height: 50,
+        margin: "auto",
+        longtitle: true,
+      },
+    };
+  },
+  methods: {
+    signIn() {
+      console.log(this.email, this.password);
+    },
+    onSuccess(googleUser) {
+      console.log(googleUser);
+      // This only gets the user information: id, name, imageUrl and email
+      console.log(googleUser.getAuthResponse().id_token);
+      console.log("dimari")
+    },
+    onFailure() {
+      console.log("Oops something wrong");
+    },
+  },
+  components: {
+    GoogleLogin,
+  },
+};
+</script>
+
+<style>
+</style>
